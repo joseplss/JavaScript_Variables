567a736f93610f78903397d42b3127f248cf1247
Jose <josepls.v@hotmail.com>
2022-04-22 12:46:24 -0700

    fixed app.js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = 'Jose';
+let lastName = 'Villalobos';
+var age = 26;
 // Exercise 2
-
+let fullName = `${firstName} ${lastName}`
+console.log(fullName)
 // Exercise 3
+let city = `San Juan Capistrano `;
+let pasttime = `gaming`;
+let myStory = `Hello! My name is ${fullName}. I live in ${city}. I enjoy ${pasttime} and coding.`;
+console.log(myStory);
